890dd854d78b57c16140dad75ec86b872c3f056e
homeejadav <homeejadav@gmail.com>
2025-04-12 03:28:38 +0530

    near perfect

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/onyxtech/', // Match your GitHub repo name
+  build: {
+    outDir: 'build', // This changes output directory to 'build'
+  }
 })
